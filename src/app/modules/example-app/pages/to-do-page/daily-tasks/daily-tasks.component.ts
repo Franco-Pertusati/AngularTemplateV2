36acf1d043cf9d6567bdd56d5b02db090bb44c55
Franco Pertusati <pertusatifranco836@gmail.com>
2025-04-28 10:27:56 -0300

    dummy componente de todoCard

--- src/app/modules/example-app/pages/to-do-page/daily-tasks/daily-tasks.component.ts
+++ src/app/modules/example-app/pages/to-do-page/daily-tasks/daily-tasks.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from "../../../../shared/button/button.component";
+import { ToDoCardComponent } from "../../../../shared/to-do-card/to-do-card.component";
 
 @Component({
   selector: 'app-daily-tasks',
   standalone: true,
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, ToDoCardComponent],
   templateUrl: './daily-tasks.component.html',
   styleUrl: './daily-tasks.component.css'
 })
