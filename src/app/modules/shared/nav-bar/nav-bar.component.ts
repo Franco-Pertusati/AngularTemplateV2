a986150733c54e3a6831cff839a55c469a11223a
Franco Pertusati <pertusatifranco836@gmail.com>
2025-05-13 20:51:31 -0300

    gaeag

--- src/app/modules/shared/nav-bar/nav-bar.component.ts
+++ src/app/modules/shared/nav-bar/nav-bar.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from "../button/button.component";
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css'
 })
